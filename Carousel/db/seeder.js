ff7914213754ee5f2dddd0847ab227ce88a6bd88
AlexAvila <alexavila.oc@gmail.com>
2020-02-15 22:37:49 -0800

    unable to save seed more than 40k(800k records) entries at a time| moving to CSV

--- Carousel/db/seeder.js
+++ Carousel/db/seeder.js
@@ -7,7 +7,8 @@ const imgUrl = 'https://hrr43teamhan.s3-us-west-1.amazonaws.com/'
 // + num.jpg
 
 // We'll test it at 50k
-let max = 50000
+let count = 0;
+let max = 3000;
 
 generateRandomNum = (min, max) => {
   min = Math.ceil(min);
@@ -26,12 +27,43 @@ let insert = async (param) => {
       // console.log(`Error inserting planet with id of ${id}`)
       // console.log(`${id}, ${randomImg}`);
     } else {
-      console.log('Success!')
+      count++
+      console.log('added to the DB',count)
       const t1 = new Date;
       console.log(t1 - t0);
+      if(count < max){
+        seeding()
+      }
     }
   })
 }
+
+
+const t0 = new Date;
+
+
+// pass more arguments
+var seeding = function () {
+  for (let instance = 1; instance <= 10; instance++) {
+    for (let imgs = 1; imgs <= 20; imgs++) {
+      let randomTag = tags[generateRandomNum(0, 2)]
+      let randomImg = imgUrl + generateRandomNum(1, 50) + '.jpg'
+      let randomDesc = faker.lorem.sentence()
+      insert([instance, randomImg, randomDesc, randomTag]);
+      //return
+    }
+  }
+}
+
+seeding()
+
+//50k will not run
+//40k will not run
+//30k will not run
+//25k will not run
+//20k might be the best option
+
+
 
 // let insertAsync = Promise.promisify(insert)
 
@@ -47,28 +79,3 @@ let insert = async (param) => {
 //       // what you want: line 41 finishes and only after it finishes you go back to line 37
 //     }
 //   }
-const t0 = new Date;
-
-
-// pass more arguments
-  // a checker to emaske
-var seeding = function (num) {
-  for (let instance = 1; instance <= num; instance++) {
-    for (let imgs = 1; imgs <= 20; imgs++) {
-      let randomTag = tags[generateRandomNum(0, 2)]
-      let randomImg = imgUrl + generateRandomNum(1, 50) + '.jpg'
-      let randomDesc = faker.lorem.sentence()
-      insert([instance, randomImg, randomDesc, randomTag]);
-      //return
-    }
-  }
-}
-
-seeding(20000)
-
-//50k will not run
-//40k will not run
-//30k will not run
-//25k will not run
-//20k might be the best option
-
